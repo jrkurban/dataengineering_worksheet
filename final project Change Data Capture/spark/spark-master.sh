a7f0175dafb9487b23420808292b990e867c8d63
jrkurban <59064403+jrkurban@users.noreply.github.com>
2023-04-05 23:37:06 +0300

    Delete final project Change Data Capture directory

--- final project Change Data Capture/spark/spark-master.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "History server is starting ...."
-
-start-history-server.sh
-
-echo "$(hostname -i) spark-master" >> /etc/hosts
-
-/opt/spark/bin/spark-class org.apache.spark.deploy.master.Master --ip 0.0.0.0 --port 7077 --webui-port 8080
--- final project Change Data Capture/spark/spark-worker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if ! getent hosts spark-master; then
-  sleep 5
-  exit 0
-fi
-
-/opt/spark/bin/spark-class org.apache.spark.deploy.worker.Worker spark://spark-master:7077 --webui-port 8081
